fe02265d9fb7d4bbc03cb5c0bbd4e9bd16b2b153
Aleks Kamko <aykamko@gmail.com>
2020-12-20 17:40:44 -0800

    mobile done

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,6 @@
     "🤡",
     "😵",
   ];
-  const animationLenMs = 8000;
   const pickRandomEmoji = () => {
     return emojiSet[Math.floor(Math.random() * emojiSet.length)];
   };
@@ -27,26 +26,47 @@
     );
 
     const containerHeight = emojiButtonContainer.scrollHeight;
-    const spreadBuffer = 400;
-    const emojiYSpread = containerHeight + spreadBuffer * 2;
     const emojiButton = document.querySelector(".emoji-button");
 
     const generateShootingEmoji = () => {
+      const shootDirection = document.body.scrollWidth < 768 ? "UP" : "RIGHT";
+
       const emojiDiv = document.createElement("div");
-      emojiDiv.className = "generated-emoji";
       emojiDiv.innerHTML = pickRandomEmoji();
-      emojiButtonContainer.appendChild(emojiDiv);
 
-      const randomYTarget = Math.round(
-        -spreadBuffer + Math.random() * emojiYSpread
-      );
+      if (shootDirection === "RIGHT") {
+        const animationLenMs = 8000;
+        emojiDiv.className = "generated-emoji left-shooting";
+        emojiButtonContainer.appendChild(emojiDiv);
 
-      requestAnimationFrame(() => {
-        emojiDiv.setAttribute(
-          "style",
-          `transition: all ${animationLenMs}ms linear; bottom: ${randomYTarget}px; left: 5000px;`
+        const spreadBuffer = 800;
+        const emojiYSpread = containerHeight + spreadBuffer * 2;
+        const randomYTarget = Math.round(
+          -spreadBuffer + Math.random() * emojiYSpread
         );
-      });
+
+        requestAnimationFrame(() => {
+          emojiDiv.setAttribute(
+            "style",
+            `transition: all ${animationLenMs}ms linear; bottom: ${randomYTarget}px; left: 5000px;`
+          );
+        });
+      } else if (shootDirection === "UP") {
+        const animationLenMs = 24000;
+        emojiDiv.className = "generated-emoji up-shooting";
+        emojiButtonContainer.appendChild(emojiDiv);
+
+        const xSpreadAmount = 4000;
+        const randomXTarget =
+          Math.round(Math.random() * xSpreadAmount * 2) - xSpreadAmount;
+
+        requestAnimationFrame(() => {
+          emojiDiv.setAttribute(
+            "style",
+            `transition: all ${animationLenMs}ms linear; bottom: 10000px; left: ${randomXTarget}px;`
+          );
+        });
+      }
 
       setTimeout(() => {
         emojiButtonContainer.removeChild(emojiDiv);
